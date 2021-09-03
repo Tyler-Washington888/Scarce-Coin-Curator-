9fef5d8c221b541b37cf3470d0a84f85b41462c5
Tyler Washington <tylerwashington@Tylers-MacBook-Air.local>
2021-09-03 08:51:14 -0400

    Not quite complete

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const BASE_URL = "http://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
 
-
+const button = document.querySelector('button');
+const input = document.querySelector('#search');
+const searchCoin = document.querySelector(".searchCoin")
+button.addEventListener('click', fetchSearch)
+async function fetchSearch() {
+  try {
+    let searchedCoin = input.value
+    let SEARCH_URL = `http://api.coingecko.com/api/v3/coins/${searchedCoin}?vs_currency=usd`
+    let res = await axios.get(`${SEARCH_URL}`, { crossdomain: true });
+    searchCoin.innerHTML = '';
+    let rightImageDiv = document.createElement('img');
+    rightImageDiv.class = 'rightImageDiv';
+    rightImageDiv.src = res.data.image.large;
+    console.log(res.data.image.large);
+    searchCoin.appendChild(rightImageDiv);
+    let h2 = document.createElement('h2');
+    h2.innerText = res.data.name;
+    searchCoin.appendChild(h2);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function getGoodData() {
   try {
@@ -20,40 +41,39 @@ getGoodData();
 
 function displayToTable(coinData) {
   let newTableRow = document.createElement('TR');
+  newTableRow.class = 'newTableRow';
   let newDataCell0 = document.createElement('TD');
-  newDataCell0.class = 'blankLeft';
+  newDataCell0.class = 'tableDataElement';
   newDataCell0.innerHTML = '';
   newTableRow.append(newDataCell0);
-
   let newDataCell1 = document.createElement('TD');
-  newDataCell1.class = 'coinRank';
+  newDataCell1.class = 'tableDataElement';
   if (coinData.market_cap_rank === undefined) {
     newDataCell1.innerHTML = " ";
   } else {
     newDataCell1.innerHTML = coinData.market_cap_rank;
   }
   newTableRow.append(newDataCell1);
-
   let newDataCell9 = document.createElement('TD');
   let coinImage = document.createElement('img');
-  coinImage.class = 'coinImage'
+  coinImage.class = 'tableDataElement'
   coinImage.src = coinData.image;
   newDataCell9.appendChild(coinImage);
   newTableRow.appendChild(newDataCell9);
   let newDataCell2 = document.createElement('TD');
-  newDataCell2.class = 'coinName';
+  newDataCell2.class = 'tableDataElement';
   newDataCell2.innerHTML = `${coinData.name} <span style="text-transform:uppercase">${coinData.symbol}</span>`;
   newTableRow.append(newDataCell2);
   let newDataCell3 = document.createElement('TD');
-  newDataCell3.class = 'coinPrice';
+  newDataCell3.class = 'tableDataElement';
   newDataCell3.innerHTML = `${'$'}${coinData.current_price}`
   newTableRow.append(newDataCell3);
   let newDataCell4 = document.createElement('TD');
-  newDataCell4.class = 'coinCirculatingSupply';
+  newDataCell4.class = 'tableDataElement';
   newDataCell4.innerHTML = coinData.circulating_supply;
   newTableRow.append(newDataCell4);
   let newDataCell5 = document.createElement('TD');
-  newDataCell5.class = 'coinTotalSupply';
+  newDataCell5.class = 'tableDataElement';
   newDataCell5.innerHTML = coinData.total_supply;
   newTableRow.append(newDataCell5);
   let newDataCell6 = document.createElement('TD');
@@ -68,40 +88,3 @@ function displayToTable(coinData) {
   tableBody.appendChild(newTableRow);
 }
 
-// function editCoinInfo() {
-//   coinList.innerHTML = ''
-//   let randomIdx = randomIndex()
-//   getGoodData(randomIdx);
-
-
-// function displayCoin(coinData) {
-  //   let coinDiv = document.createElement("div");
-  //   let coinImage = document.createElement('img');
-  //   coinImage.src = coinData.image;
-  //   coinDiv.append(coinImage);
-  //   let coinNameH1 = document.createElement('h1');
-  //   coinNameH1.innerHTML = coinData.name;
-  //   coinDiv.append(coinNameH1);
-  //   let coinSymbolH2 = document.createElement('h2');
-  //   coinSymbolH2.innertext = coinData.symbol;
-  //   coinDiv.append(coinSymbolH2);
-  //   let coinPriceH1 = document.createElement("h1");
-  //   coinPriceH1.innerHTML = "$ " + coinData.current_price;
-  //   coinDiv.append(coinPriceH1);
-  //   let circulatingSupplyH1 = document.createElement('h1');
-  //   circulatingSupplyH1.innerHTML = coinData.circulating_supply;
-  //   coinDiv.append(circulatingSupplyH1);
-  //   coinList.append(coinDiv);
-  // }
-
-  // let coinList = document.querySelector('.coinList');
-
-  // let button = document.querySelector('button');
-  // button.addEventListener('click', editCoinInfo)
-
-  // function randomIndex() {
-  //   return Math.floor(Math.random() * 100)
-  // }
-
-
-// }
